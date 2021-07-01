ca6628bccf019e1e4b8646fca0a537ff2912c267
Bhavini <74909895+Bhavini20@users.noreply.github.com>
2021-07-01 22:15:55 +0530

    Add files via upload

--- shopping.js
+++ shopping.js
@@ -1,24 +1,45 @@
 var express = require('express');
 var path = require('path');
 var mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/shopcart');
+var config = require('./config/database.js');
+const bodyParser = require('body-parser');
+var session = require('express-session');
+var expressValidator = require('express-validator');
+//connect to databse
+mongoose.connect(config.database);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
     console.log("Connected to db");
 });
-var app = express();
 
+//init app
+var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res){
-    res.send('Working');
-});
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+}));
+
+//add middleware for express validator vid 7
+
+//set routes
+var pages = require('./routes/pages.js');
+var adminpages = require('./routes/adminpages.js');
+app.use('/', pages);
+app.use('/admin/pages',adminpages);
 
 var port=3000;
 app.listen(port, function(){
